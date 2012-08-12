011ef8b3c5f10297b391424e17cf705c737e6717
Andraz Brodnik <brodul@brodul.org>
2012-08-12 22:50:00 +0200

    Added the setup.py file.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='githook',
+      version=version,
+      description="Simple Flask aplication that runs a script in response of GitHub post hook.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Andraz Brodnik',
+      author_email='brodul@brodul.org',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+            "Flask",
+            "mock",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
