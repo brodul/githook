3c13e8dc555d4b1e26e9cb5c433c4faa793e1070
Andraz Brodnik <brodul@brodul.org>
2012-08-22 12:25:18 +0200

    Setup py meta data changes.

--- setup.py
+++ setup.py
@@ -4,14 +4,23 @@ version = '0.1'
 
 setup(name='githook',
       version=version,
-      description="Simple Flask aplication that runs a script in response of GitHub post hook.",
+      description="Simple web application that runs a script in response of GitHub post hook.",
       long_description="""\
+      This aplication starts a small web server, 
+reads a INI config file and listens for GitHub post requests,
+then runs a script in response of the post request.
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: POSIX',
+                   'Topic :: Utilities'
+                  ],
       keywords='',
       author='Andraz Brodnik',
       author_email='brodul@brodul.org',
-      url='',
+      url='https://github.com/brodul/githook',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
@@ -19,7 +28,6 @@ setup(name='githook',
       install_requires=[
           # -*- Extra requirements: -*-
             "Flask",
-            "mock",
       ],
       entry_points={'console_scripts': ['githook = githook:cli_run'] },
       )
